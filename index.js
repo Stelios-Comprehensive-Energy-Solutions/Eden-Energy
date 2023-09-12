4c7ac995d60f5beb2d5830ea55d3adbcffe88b01
Raynhardt Van Zyl <raynhardt.vanzyl@gmail.com>
2023-09-12 09:24:28 +0200

    Latest

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const navList = document.querySelector('.nav-list');
 
 menuToggle.addEventListener('click', () => {
     navList.classList.toggle('active');
+    menuToggle.classList.toggle('active');  // Add this line
 });
 
 // Smooth Scrolling
